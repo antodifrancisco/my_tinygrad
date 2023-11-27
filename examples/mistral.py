02f55e02328f2d1db0b3a5c15404994f36f21d6d
antodifrancisco <101689275+antodifrancisco@users.noreply.github.com>
2023-11-27 04:29:35 -0800

    Loading weights

--- examples/mistral.py
+++ examples/mistral.py
@@ -1,4 +1,9 @@
+import os, sys
+sys.path.append(os.getcwd())
+
 from tinygrad import Tensor, nn
+from tinygrad.helpers import Timing
+from examples.llama import Transformer, convert_from_huggingface
 
 """
 model.layers.0.self_attn.q_proj.weight (4096, 4096)
@@ -16,8 +21,18 @@ model.layers.0.post_attention_layernorm.weight (4096,)
 if __name__ == "__main__":
   Tensor.no_grad = True
   # TODO: add read only Tensors
-  part1 = nn.state.torch_load("../weights/OpenHermes/pytorch_model-00001-of-00002.bin")
-  part2 = nn.state.torch_load("../weights/OpenHermes/pytorch_model-00002-of-00002.bin")
+  with Timing("load weights: "):
+    part1 = nn.state.torch_load("weights/OpenHermes/pytorch_model-00001-of-00002.bin")
+    part2 = nn.state.torch_load("weights/OpenHermes/pytorch_model-00002-of-00002.bin")
 
-  for k,v in part1.items():
-    print(k,v.shape)
+  with Timing("create model: "):
+    model = Transformer(4096, 14336, n_heads=32, n_layers=32, norm_eps=1e-5, vocab_size=32002, n_kv_heads=8)
+
+  with Timing("weights -> model: "):
+    weights = convert_from_huggingface(part1, model, 32, 8)
+    nn.state.load_state_dict(model, weights, strict=False)
+    weights = convert_from_huggingface(part2, model, 32, 8)
+    nn.state.load_state_dict(model, weights, strict=False)
+
+  # for k,v in part1.items():
+  #   print(k,v.shape)
